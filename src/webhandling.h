3816c9a2499a0b6b244d9d8d5580168ca4901123
Andy <azogg65@bluewin.ch>
2023-12-31 09:00:21 +0100

    removed not supported temp sources

--- src/webhandling.h
+++ src/webhandling.h
@@ -20,42 +20,43 @@
 #define STRING_LEN 128
 
 static char TempSourceValues[][STRING_LEN] = {
-    "0",
+//    "0",
     "1",
     "2",
     "3",
     "4",
-    "5",
-    "6",
+//    "5",
+//    "6",
     "7",
     "8",
-    "9",
-    "10",
-    "11",
-    "12",
+//    "9",
+//    "10",
+//    "11",
+//    "12",
     "13",
     "14",
-    "15"
+//    "15"
 };
 
 static char TempSourceNames[][STRING_LEN] = {
-    "Sea water temperature",
+ //   "Sea water temperature",
     "Outside temperature",
     "Inside temperature",
     "Engine room temperature",
     "Main cabin temperature",
-    "Live well temperature",
-    "Bait well temperature",
+ //   "Live well temperature",
+ //   "Bait well temperature",
     "Refrigeration temperature",
     "Heating system temperature",
-    "Dew point temperature",
-    "Apparent wind chill temperature",
-    "Theoretical wind chill temperature",
-    "Heat index temperature",
+//    "Dew point temperature",
+//    "Apparent wind chill temperature",
+//    "Theoretical wind chill temperature",
+//    "Heat index temperature",
     "Freezer temperature",
     "Exhaust gas temperature",
-    "Shaft seal temparature"
+//    "Shaft seal temparature"
 };
+
 
 #define HTML_Start_Doc "<!DOCTYPE html>\
     <html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>\
