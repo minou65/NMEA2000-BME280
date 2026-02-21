650d57605fef0192287e25b002aabb7edd485f1c
Andy <azogg65@bluewin.ch>
2026-02-21 08:17:55 +0100

    new favicon

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 3
-#define VERSION_PATCH 0
+#define VERSION_PATCH 1
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 27
-#define VERSION_DATE "2026-01-09"
-#define VERSION_TIME "11:55:15"
-#define VERSION "1.3.0.27"
-#define VERSION_STR "1.3.0.27 (2026-01-09 11:55:15)"
+#define VERSION_BUILD 30
+#define VERSION_DATE "2026-02-21"
+#define VERSION_TIME "08:16:49"
+#define VERSION "1.3.1.30"
+#define VERSION_STR "1.3.1.30 (2026-02-21 08:16:49)"
